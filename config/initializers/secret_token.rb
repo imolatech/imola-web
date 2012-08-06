caefe478448700dc353d59ce1638a70353fcbaee
wenhu guan <guanwenhu@hotmail.com>
2012-08-05 22:15:06 -0700

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imolaweb::Application.config.secret_token = '1b891df2832878b972065ce4b959637676d55524954f3ecaa9ec01cdfb607e911eb50595c86044295e7e5d5e44c396126c019059cd3d1ac96cdb2fc29b1bc087'
