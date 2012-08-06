3b874b4fa6f889379537b54d16407afd3716346e
wenhu guan <guanwenhu@hotmail.com>
2012-08-06 00:24:56 -0700

    first time customization

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '>= 1.0.3'
+  gem 'font-awesome-sass-rails'
 end
 gem 'jquery-rails'
 gem "rspec-rails", ">= 2.11.0", :group => [:development, :test]
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
+user = User.create! :name => 'First User', :email => 'user@imola.com', :password => 'changeit', :password_confirmation => 'changeit'
 puts 'New user created: ' << user.name
-user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
+user2 = User.create! :name => 'Admin User', :email => 'admin@imola.com', :password => 'changeit', :password_confirmation => 'changeit'
 puts 'New user created: ' << user2.name
-user.add_role :admin
+user2.add_role :admin
